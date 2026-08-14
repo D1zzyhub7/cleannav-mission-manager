0045805abd2d8c450c54bc6ce15ce500924b03db
d1zzy <d1zzy@localhost>
2026-08-14 21:13:59 +0800

    建立 Mission Manager M1 最小包骨架

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'cleannav_mission_manager'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='CleanNav Team',
+    maintainer_email='1339980053@qq.com',
+    description='CleanNav Mission Manager M1 mock implementation',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
